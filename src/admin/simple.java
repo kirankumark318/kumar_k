681f6191b4d47e1500cd6119939262ff4054a4bf
Hp <Hp@DESKTOP-SITFNC3>
2022-11-09 20:59:26 +0530

    hi dis is 2nd commit

--- src/admin/simple.java
+++ src/admin/simple.java
@@ -4,4 +4,10 @@ public class simple {
 public static void main(String[] args) {
 	System.out.println("hiii");
 }
+	public void simple2()
+	{
+	    System.out.println("helloooo");	
+	    System.out.println("jiii");
+	}
+
 }
